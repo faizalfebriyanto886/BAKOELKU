b268eed3ca6cd0d1c5fed92f72e1db643e2b4f34
faizalfebriyanto886 <faizalfebriyanto886@gmail.com>
2023-01-26 17:12:55 +0700

    improve register

--- lib/screen/daftar/controller/register_controller.dart
+++ lib/screen/daftar/controller/register_controller.dart
@@ -1,10 +1,14 @@
 import 'dart:async';
+import 'dart:io';
+import 'dart:math';
 import 'package:bakoelku/reusable_widget/alert_widget.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:get/get.dart';
+import 'package:image_picker/image_picker.dart';
 
 class RegisterController extends GetxController {
   final usernameController = TextEditingController();
@@ -12,7 +16,12 @@ class RegisterController extends GetxController {
   final passwordController = TextEditingController();
   final noTelpController = TextEditingController();
   final alamatController = TextEditingController();
+  final namaGerobakController = TextEditingController();
   final roleController = ''.obs;
+  final validationRole = false.obs;
+  final ImagePicker picker = ImagePicker();
+  File? urlImages;
+  String urlImageStorage = '';
 
   clearRegisterData() {
     usernameController.clear();
@@ -25,7 +34,7 @@ class RegisterController extends GetxController {
   
   CollectionReference userData = FirebaseFirestore.instance.collection("auth");
 
-  registrasiUser(BuildContext context) async {
+  registrasiUserPedagang(BuildContext context) async {
     if (
       usernameController.text.isNotEmpty 
       && emailController.text.isNotEmpty 
@@ -46,7 +55,11 @@ class RegisterController extends GetxController {
           'uid': value.user!.uid,
           'alamat': alamatController.text,
           'role': roleController.value,
-          'latlong': GeoPoint(posisi.latitude, posisi.longitude)
+          'latlong': GeoPoint(posisi.latitude, posisi.longitude),
+          'nama_gerobak': namaGerobakController.text,
+          'foto_gerobak': FieldValue.arrayUnion([
+            urlImageStorage
+          ])
         }).then((value) {
           clearRegisterData();
           CustomAlertDialogSuccess(
@@ -54,7 +67,10 @@ class RegisterController extends GetxController {
             subTitle: "Email dan password berhasil terdaftar", 
             context: context
           );
-          Timer(const Duration(seconds: 3), () => Get.back());
+          Timer(const Duration(seconds: 3), () {
+            Get.back();
+            Get.back();
+          });
         }).catchError((error) {
           CustomAlertDialogWarning(
             title: "Maaf",
@@ -69,6 +85,111 @@ class RegisterController extends GetxController {
         subTitle: "terjadi kesalahan, Silahkan cek kembali", 
         context: context
       );
+    }
+  }
+
+  registrasiUserPembeli(BuildContext context) async {
+    if (
+      usernameController.text.isNotEmpty 
+      && emailController.text.isNotEmpty 
+      && passwordController.text.isNotEmpty 
+      && noTelpController.text.isNotEmpty
+      && alamatController.text.isNotEmpty
+      && roleController.value.isNotEmpty
+    ) {
+      FirebaseAuth.instance.createUserWithEmailAndPassword(
+        email: emailController.text, 
+        password: passwordController.text,
+      ).then((value) async {
+        Position posisi = await getGeoLocationPosition();
+        userData.doc(value.user?.uid).set({
+          'email': emailController.text,
+          'name': usernameController.text,
+          'no_telp': double.parse(noTelpController.text),
+          'uid': value.user!.uid,
+          'alamat': alamatController.text,
+          'role': roleController.value,
+          'latlong': GeoPoint(posisi.latitude, posisi.longitude),
+          'nama_gerobak': namaGerobakController.text,
+          'foto_gerobak': FieldValue.arrayUnion([
+            urlImageStorage
+          ])
+        }).then((value) {
+          clearRegisterData();
+          CustomAlertDialogSuccess(
+            title: "Berhasil", 
+            subTitle: "Email dan password berhasil terdaftar", 
+            context: context
+          );
+          Timer(const Duration(seconds: 3), () {
+            Get.back();
+          });
+        }).catchError((error) {
+          CustomAlertDialogWarning(
+            title: "Maaf",
+            subTitle: "Email dan password tidak berhasil terdaftar, mohon cek kembali", 
+            context: context
+          );
+        });
+      });
+    } else {
+      CustomAlertDialogWarning(
+        title: "Oops!", 
+        subTitle: "terjadi kesalahan, Silahkan cek kembali", 
+        context: context
+      );
+    }
+  }
+
+  Future uploadImageGallery(BuildContext context) async {
+    final pickerFile = await picker.pickImage(source: ImageSource.gallery);
+
+    if (pickerFile != null) {
+      urlImages = File(pickerFile.path);
+      CustomAlertDialogSuccess(
+        title: "Success", 
+        subTitle: "Gambar Berhasil ditambahkan", 
+        context: context
+      );
+
+      update();
+      
+    } else {
+      // ignore: avoid_print
+      print("error Gallery");
+    }
+    update();
+  }
+
+  Future uploadToStorage(String namaPedagang, String namaGerobak, BuildContext context) async {
+    var rng = Random();
+    var angka = List.generate(1, (_) => rng.nextInt(10000));
+    var destination = 'Image_gerobak/$namaPedagang';
+
+    try {
+      final ref = FirebaseStorage.instance
+          .ref(destination)
+          .child('$namaGerobak $angka/');
+      await ref.putFile(urlImages!);
+      
+      String urlDownload = await ref.getDownloadURL();
+      // urlDownload == urlImageStorage;
+      urlImageStorage = urlDownload;
+      
+      // FirebaseFirestore.instance.collection('auth').doc(uid).update({
+      //   'foto_gerobak': FieldValue.arrayUnion([
+      //     urlDownload,
+      //   ])
+      // });
+      CustomAlertDialogSuccess(
+        title: "Success", 
+        subTitle: "Gambar Berhasil diupload", 
+        context: context
+      );
+      update();
+    } catch (e) {
+      // ignore: avoid_print
+      print(e);
     }
   }
 
---
+++ lib/screen/daftar/view/register_page_gerobak.dart
@@ -0,0 +1,235 @@
+
+import 'package:bakoelku/colors.dart';
+import 'package:bakoelku/screen/daftar/controller/register_controller.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import '../../../reusable_widget/custom_text_field.dart';
+
+class RegisterPageGerobak extends StatelessWidget {
+  final controller = Get.put(RegisterController());
+  RegisterPageGerobak({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: backdoor,
+      body: Stack(
+        children: [
+          Container(
+            padding: const EdgeInsets.only(top: 80),
+            alignment: Alignment.topCenter,
+            child: Image.asset("assets/images/logo_bakoelku.png", height: 120,),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              padding: const EdgeInsets.fromLTRB(20, 20, 20, 25),
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(20),
+                  topRight: Radius.circular(20)
+                )
+              ),
+              child: SingleChildScrollView(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Text(
+                      "Registrasi",
+                      style: TextStyle(
+                        color: primary,
+                        fontSize: 26,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    Column(
+                      children: [
+                        Padding( // text Field for username
+                          padding: const EdgeInsets.only(left: 10, right: 10),
+                          child: CustomTextFieldWithState(
+                            controller: controller.namaGerobakController,
+                            color: primary, 
+                            hintText: "Isi Nama Gerobak kamu disini...", 
+                            prefix: Icon(Icons.shopping_cart, color: primary)
+                          )
+                        ),
+                        const SizedBox(height: 20),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 10, right: 10),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              GestureDetector(
+                                onTap: () {
+                                  bottomSheetUploadImage(context);
+                                },
+                                child: Container(
+                                  height: 55,
+                                  padding: const EdgeInsets.only(left: 10, right: 10),
+                                  width: Get.width * 0.7,
+                                  decoration: BoxDecoration(
+                                    color: Colors.white,
+                                    border: Border.all(
+                                      width: 2,
+                                      color: primary
+                                    ),
+                                    borderRadius: BorderRadius.circular(15)
+                                  ),
+                                  child: Row(
+                                    children: [
+                                      Image.asset('assets/icon/upload-image.png', height: 25),
+                                      const SizedBox(width: 15),
+                                      Text(
+                                        "Unggah Foto Gerobak",
+                                        style: TextStyle(
+                                          color: Colors.grey[500],
+                                          fontSize: 14,
+                                          fontWeight: FontWeight.w500
+                                        ),
+                                      )
+                                    ],
+                                  ),
+                                ),
+                              ),
+
+                              GestureDetector(
+                                onTap: () {
+                                  controller.uploadToStorage(controller.usernameController.text, controller.namaGerobakController.text, context);
+                                },
+                                child: Container(
+                                  padding: const EdgeInsets.all(10),
+                                  decoration: BoxDecoration(
+                                    color: Colors.white,
+                                    shape: BoxShape.circle,
+                                    border: Border.all(
+                                      width: 2,
+                                      color: primary
+                                    )
+                                  ),
+                                  child: Icon(Icons.file_upload_outlined, color: primary,),
+                                ),
+                              )
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+
+                    const SizedBox(height: 30),
+
+                    Padding(
+                      padding: const EdgeInsets.only(left: 10, right: 10),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          GestureDetector(
+                            onTap: () => Get.back(),
+                            child: Container(
+                              alignment: Alignment.center,
+                              height: 45,
+                              width: Get.width * 0.4,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10),
+                                color: danger
+                              ),
+                              child: const Text(
+                                "Kembali",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.w500,
+                                  fontSize: 20
+                                ),
+                              ),
+                            ),
+                          ),
+                          GestureDetector(
+                            onTap: () {
+                              controller.registrasiUserPedagang(context);
+                            },
+                            child: Container(
+                              alignment: Alignment.center,
+                              height: 45,
+                              width: Get.width * 0.4,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10),
+                                color: primary
+                              ),
+                              child: const Text(
+                                "Daftar",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.w500,
+                                  fontSize: 20
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  bottomSheetUploadImage(BuildContext context) {
+    Get.bottomSheet(
+      Container(
+        padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 25),
+        decoration: const BoxDecoration(
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(10),
+            topRight: Radius.circular(10),
+          ),
+          color: Colors.white
+        ),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            GestureDetector(
+              onTap: () {
+                // controller.uploadImageCamera();
+                Get.back();
+              },
+              child: Container(
+                alignment: Alignment.center,
+                height: 50,
+                width: Get.width,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  color: primary
+                ),
+                child: const Text("Kamera", style: TextStyle(color: Colors.white, fontSize: 18, fontWeight: FontWeight.w500),),
+              ),
+            ),
+            const SizedBox(height: 20),
+            GestureDetector(
+              onTap: () {
+                controller.uploadImageGallery(context);
+                Get.back();
+              },
+              child: Container(
+                height: 50,
+                width: Get.width,
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  color: primary
+                ),
+                child: const Text("Gallery", style: TextStyle(color: Colors.white, fontSize: 18, fontWeight: FontWeight.w500),),
+              ),
+            ),
+          ],
+        ),
+      )
+    );
+  }
+}
